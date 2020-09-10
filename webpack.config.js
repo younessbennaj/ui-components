c1beef59ccad009e6a242c12306e64a2c628294e
Youness BENNAJ <youness.bennaj@gmail.com>
2020-09-10 16:13:41 +0200

    set up style

--- webpack.config.js
+++ webpack.config.js
@@ -20,6 +20,10 @@ module.exports = {
                         presets: ['@babel/preset-react']
                     }
                 }
+            },
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
             }
 
         ]
