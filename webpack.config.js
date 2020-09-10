1d027dead54926eb362158eb2345f79caa26927c
Youness BENNAJ <youness.bennaj@gmail.com>
2020-09-10 16:01:36 +0200

    setup babel

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,26 @@
 const path = require("path");
 
 module.exports = {
+    mode: "development",
     entry: "./src/index.js",
     output: {
         //need an absolute path
         path: path.resolve(__dirname, "dist"),
         filename: "bundle.js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-react']
+                    }
+                }
+            }
+
+        ]
     }
 }
